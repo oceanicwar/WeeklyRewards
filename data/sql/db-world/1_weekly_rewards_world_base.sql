00a399e385f3158594c06f26148ef6c33067b506
jshplayer <jshplayer@outlook.com>
2024-02-16 16:28:39 +1300

    Updating queries for OceWar

---
+++ data/sql/db-world/1_weekly_rewards_world_base.sql
@@ -0,0 +1,2 @@
+DELETE FROM `game_event` WHERE `eventEntry`=123;
+INSERT INTO `game_event` (`eventEntry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `holidayStage`, `description`, `world_event`, `announce`) VALUES (123, '2024-02-11 20:00:00', '2999-01-01 20:00:00', 10080, 1, 0, 0, 'Weekly Sunday Rewards', 0, 2);
--- data/sql/db-world/base/weekly_rewards_0000_00_00_00.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `game_event` WHERE `eventEntry`=123;
-INSERT INTO `game_event` (`eventEntry`, `start_time`, `end_time`, `occurence`, `length`, `holiday`, `holidayStage`, `description`, `world_event`, `announce`) VALUES (123, '2024-02-08 20:00:00', '2031-01-02 02:00:00', 10080, 1, 0, 0, 'Weekly Rewards Flush', 0, 2);
