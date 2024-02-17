4254c6975aa3af32361b4b322c0c5acad7fded32
jshplayer <jshplayer@outlook.com>
2024-02-17 17:04:13 +1300

    OceWar additions

---
+++ data/sql/db-characters/2_weekly_rewards_char_rewards.sql
@@ -0,0 +1,3 @@
+DELETE FROM `character_weekly_rewards`;
+INSERT INTO `character_weekly_rewards` (`item_entry`, `count`, `max_count`, `multiplier`, `note`) VALUES
+	(37711, 10, 999999, 1, 'Oceanic War Coin');
