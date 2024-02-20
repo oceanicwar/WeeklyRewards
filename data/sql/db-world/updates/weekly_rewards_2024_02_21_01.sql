2923e0372641275786ea5d7317cf8df8cd9fce16
AnchyDev <lewienato@gmail.com>
2024-02-21 08:55:32 +1100

    Added Tempest Keep weapons and council bosses to blacklist.

---
+++ data/sql/db-world/updates/weekly_rewards_2024_02_21_01.sql
@@ -0,0 +1,12 @@
+DELETE FROM `weekly_activity_creature_blacklist` WHERE `entry`=20060;
+INSERT INTO `weekly_activity_creature_blacklist` (`entry`, `comment`) VALUES (20060, 'Tempest Keep - Kael\'Thas - Lord Sanguinar');
+DELETE FROM `weekly_activity_creature_blacklist` WHERE `entry`=20062;
+INSERT INTO `weekly_activity_creature_blacklist` (`entry`, `comment`) VALUES (20062, 'Tempest Keep - Kael\'Thas - Grand Astromancer Capernian');
+DELETE FROM `weekly_activity_creature_blacklist` WHERE `entry`=20063;
+INSERT INTO `weekly_activity_creature_blacklist` (`entry`, `comment`) VALUES (20063, 'Tempest Keep - Kael\'Thas - Master Engineer Telonicus');
+DELETE FROM `weekly_activity_creature_blacklist` WHERE `entry`=20064;
+INSERT INTO `weekly_activity_creature_blacklist` (`entry`, `comment`) VALUES (20064, 'Tempest Keep - Kael\'Thas - Thaladred the Darkener');
+DELETE FROM `weekly_activity_creature_blacklist` WHERE `entry`=21273;
+INSERT INTO `weekly_activity_creature_blacklist` (`entry`, `comment`) VALUES (21273, 'Tempest Keep - Kael\'Thas - Phaseshift Bulwark');
+DELETE FROM `weekly_activity_creature_blacklist` WHERE `entry`=21268;
+INSERT INTO `weekly_activity_creature_blacklist` (`entry`, `comment`) VALUES (21268, 'Tempest Keep - Kael\'Thas - Netherstrand Longbow');
